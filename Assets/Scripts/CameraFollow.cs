de3d61c7cef9b1c545db564fa2c15ef6c56b5297
Moose <DylanDixon-@outlook.com>
2025-01-14 21:50:51 -0400

    Camera and Jump
    Added camera smoothing as well as setting limits for how far left, right, up, and down it can go. Adjusted the jump to have increased gravity while falling to aid in the jump feeling less 'floaty'. Added collision checkers in player to check if the player is on the ground so that the play can only jump when they are 'grounded'

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,46 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{   
+    public GameObject Player;   
+    public float timeOffset;
+    public Vector2 posOffset;
+
+    public float leftLimit;
+    public float rightLimit;
+    public float bottomLimit;
+    public float topLimit;
+
+    private Vector3 velocity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //Camera current position
+        Vector3 startPos = transform.position;
+
+      //Player current position
+        Vector3 endPos = Player.transform.position;
+
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z = -10;
+
+        //Smoothly move the camera towards player position
+        transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+
+        transform.position = new Vector3
+            (
+                Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+                Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+                transform.position.z
+            );
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,81 @@
 using UnityEngine;
 
+//Creates rigidbody2d attached to whatever this script is attached to and prevents removal of rigidbody2d from object.
+//ReuireComponent can only contain 3 in a single statement, additonal lines would be required for more.
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
 public class PlayerController : MonoBehaviour
 {
+    /*public makes it available in unity to change from in engine and not just from script
+      having [x] applies to the below var. In this case setting the range that the player speed can be.*/
+    [Range(3, 10)]
+    public float speed = 5.0f;
+    public float jumpForce = 10.3f;
+    [Range(1, 10)]
+    public float fallSpeed = 1.0f;
+
+    bool grounded;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Assigning unity Rigidbody2D to var.
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        rb.linearVelocity = Vector3.zero;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Movement for left and right.
+        float hInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(hInput * speed, rb.linearVelocity.y);
+
+        if (rb.linearVelocity.x > 0) {
+            sr.flipX = false; }
+        if (rb.linearVelocity.x < 0) {
+            sr.flipX = true; }
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+ //This makes the player fall faster over time so the jump feels less floaty
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallSpeed - 1) * Time.deltaTime;
+        }
+    }
+    // Called when the player's collider starts touching another collider
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the player is touching the ground
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    // Called while the player's collider stays in contact with another collider
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    // Called when the player's collider stops touching another collider
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            grounded = false;
+        }
     }
 }
